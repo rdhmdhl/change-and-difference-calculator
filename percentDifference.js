40b7d31a08ef3365dc92a7578878fdfd7d1192df
rdhmdhl <reid@wearthefewture.com>
2022-09-13 16:51:02 -0700

    add parseFloat() to difference calculator. add Math.abs() to differenceResults. add .gitignore with node_modules/

--- percentDifference.js
+++ percentDifference.js
@@ -1,9 +1,26 @@
-document.getElementById("getDifferenceResults").onclick = function() {
-    const x = document.getElementById("valueX").value;
-    const y = document.getElementById("valueY").value;
-    const differenceResults = ((x - y) / ((x + y) / 2)) * 100 
 
-    document.getElementById("differenceResults").innerHTML = 
+document.getElementById("getDifferenceResults").onclick = function() {
+    
+    const valueOne = document.getElementById("valueOne").value;
+    const valueTwo = document.getElementById("valueTwo").value;
+    // valueOne = 1;
+    // valueTwo = 2;
+
+    const topValue = (parseFloat(valueOne) - parseFloat(valueTwo));
+
+
+    const bottomValue = ((parseFloat(valueOne) + parseFloat(valueTwo)) / 2);
+ 
+
+    const differenceResults = Math.abs((topValue / bottomValue) * 100);
+
+
+
+    document.getElementById("insertDifferenceResults").innerHTML = 
     (parseFloat(differenceResults).toFixed(2)+ "%");
+
+    console.log(valueOne, valueTwo);
+    console.log(topValue, bottomValue);
+    console.log(differenceResults);
 }
 
